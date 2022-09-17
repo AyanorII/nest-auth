ef7e15deeac0a5482563cd4daad1caefd72b7898
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-09-17 22:48:09 +0900

    add signUp

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(201)
+  @Post('/signup')
+  async signUp(@Body() authCredentials: AuthCredentialsDto) {
+    return this.authService.signUp(authCredentials);
+  }
+}
